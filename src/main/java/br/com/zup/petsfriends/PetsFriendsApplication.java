79ac243b33263fd8fdff339dacb954db74dfb21a
Vinícius <vinicius.raymundo@zup.com.br>
2021-03-15 13:37:58 -0300

    Criar projeto Pets Friend

---
+++ src/main/java/br/com/zup/petsfriends/PetsFriendsApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.petsfriends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetsFriendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetsFriendsApplication.class, args);
+	}
+
+}

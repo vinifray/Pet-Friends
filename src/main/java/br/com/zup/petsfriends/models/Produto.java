03a6fd49df094342617543e6d57298c01b9b2f36
Vinícius <vinicius.raymundo@zup.com.br>
2021-03-15 15:21:44 -0300

    Criar classe model de Produto

---
+++ src/main/java/br/com/zup/petsfriends/models/Produto.java
@@ -0,0 +1,34 @@
+package br.com.zup.petsfriends.models;
+
+public class Produto {
+    private String nome;
+    private double preco;
+    private int quantidade;
+
+    public Produto() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+}

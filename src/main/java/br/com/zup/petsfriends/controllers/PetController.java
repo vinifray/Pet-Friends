03d4d932fe17bfa404eb3436677f8e4e76e61d04
Vinícius <vinicius.raymundo@zup.com.br>
2021-03-15 15:21:19 -0300

    Implementa pesquisa de pet por email

--- src/main/java/br/com/zup/petsfriends/controllers/PetController.java
+++ src/main/java/br/com/zup/petsfriends/controllers/PetController.java
@@ -1,9 +1,11 @@
 package br.com.zup.petsfriends.controllers;
 
 import br.com.zup.petsfriends.dtos.CadastroPetDTO;
+import br.com.zup.petsfriends.dtos.ResumoPetDTO;
 import br.com.zup.petsfriends.models.Pet;
 import br.com.zup.petsfriends.services.PetService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,10 +16,16 @@ public class PetController {
     private PetService petService;
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public Pet cadastrarPet(@RequestBody CadastroPetDTO cadastroPetDTO){
         Pet pet = cadastroPetDTO.converterDTOParaPet();
         return petService.cadastrarPet(pet);
     }
 
+    @GetMapping("{email}/")
+    public ResumoPetDTO pesquisarPetPorEmail(@PathVariable String email){
+        Pet pet = petService.pesquisarPeloEmailDoDono(email);
+        return ResumoPetDTO.converterPetParaDTO(pet);
+    }
 
 }

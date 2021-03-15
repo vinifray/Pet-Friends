912c68b082680bfed7f32274adc5d051aa675245
Vinícius <vinicius.raymundo@zup.com.br>
2021-03-15 13:54:32 -0300

    Criar metodo de cadastro para pets

---
+++ src/main/java/br/com/zup/petsfriends/services/PetService.java
@@ -0,0 +1,21 @@
+package br.com.zup.petsfriends.services;
+
+import br.com.zup.petsfriends.models.Pet;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class PetService {
+    private static List<Pet> pets = new ArrayList<>();
+
+    public Pet cadastrarPet(Pet pet){
+        pet.setDataEntrada(LocalDate.now());
+        pets.add(pet);
+
+        return pet;
+    }
+
+}

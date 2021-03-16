b94a9301313ec74332468fd090a7e513ea247180
Weslley Lima <weslley.lima@zup.com.br>
2021-03-16 16:33:34 -0300

    Criar metodo deletarNomeDoDono na service na PetController

--- src/main/java/br/com/zup/petsfriends/services/PetService.java
+++ src/main/java/br/com/zup/petsfriends/services/PetService.java
@@ -36,4 +36,19 @@ public class PetService {
         pets.remove(pets);
     }
 
+    public void deletarNomeDono(String nome) throws RuntimeException{
+        Pet estimacao = new Pet();
+        estimacao.setNome(nome);
+        deletarNomeDono(estimacao);
+    }
+
+    public boolean deletarNomeDono(Pet pet) {
+        for(Pet animalEstimacao: pets){
+            if(animalEstimacao.getNome().equalsIgnoreCase(animalEstimacao.getNome())){
+                pets.remove(animalEstimacao);
+                return true;
+            }
+        }
+        throw new RuntimeException("Nome não encontrado!");
+    }
 }
